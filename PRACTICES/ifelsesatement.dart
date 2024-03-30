e2859dffd4ef2d44a32566f0199b6783e310932d
2024git <siayiphoebejepchumba@gmail.com>
2024-03-30 12:44:51 +0300

    addition

---
+++ PRACTICES/ifelsesatement.dart
@@ -0,0 +1,11 @@
+void main() {
+  var temperature = 25;
+
+  if (temperature > 30) {
+    print('It\'s a hot day!');
+  } else if (temperature > 20) {
+    print('It\'s a warm day.');
+  } else {
+    print('It\'s a cold day.');
+  }
+}
---
+++ PRACTICES/switchstatement.dart
@@ -0,0 +1,17 @@
+void main() {
+  var grade = 'A';
+
+  switch (grade) {
+    case 'A':
+      print('Excellent!');
+      break;
+    case 'B':
+      print('Good!');
+      break;
+    case 'C':
+      print('Fair!');
+      break;
+    default:
+      print('Needs Improvement.');
+  }
+}
